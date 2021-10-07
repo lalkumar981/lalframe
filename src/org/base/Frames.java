862e44241bb8476d3f54c8a013971770a540279d
lalkumar981 <lalkumar981@gmail.com>
2021-10-07 23:28:24 +0530

    jk

--- src/org/base/Frames.java
+++ src/org/base/Frames.java
@@ -10,7 +10,9 @@ public class Frames {
 private void tc1() {
 	System.out.println("tc1");
 	
-
+}
+private void tc2() {
+	System.out.println("tc2");
 }
 	public static void main(String[] args) {
 		
