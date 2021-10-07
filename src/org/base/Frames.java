21b4b88d48bd315b447b1fe9708c163175fc097c
lalkumar981 <lalkumar981@gmail.com>
2021-10-07 23:07:35 +0530

    2

--- src/org/base/Frames.java
+++ src/org/base/Frames.java
@@ -6,6 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Frames {
+	
+private void tc1() {
+	System.out.println("tc1");
+	
+
+}
 	public static void main(String[] args) {
 		
 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\BasicSelenium\\driver\\chromedriver.exe");
