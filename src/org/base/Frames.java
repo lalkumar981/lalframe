03830297b9c155bd576d1eab8eed6948a56e36c0
lalkumar981 <lalkumar981@gmail.com>
2021-10-07 22:50:33 +0530

    1

---
+++ src/org/base/Frames.java
@@ -0,0 +1,39 @@
+package org.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Frames {
+	public static void main(String[] args) {
+		
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\BasicSelenium\\driver\\chromedriver.exe");
+		
+		WebDriver driver =new ChromeDriver();
+		 driver.get("https://www.redbus.in/info/redcare");
+		 
+		 driver.manage().window().maximize();
+		 
+		 //Switch to frame
+		 WebElement element = driver.findElement(By.xpath("//iframe[@class='modalIframe']"));
+		 
+		 driver.switchTo().frame(element);
+		 
+		 WebElement hd = driver.findElement(By.xpath("//input[@id='mobileNoInp']"));
+		 hd.sendKeys("9600539757");
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
